2abdfa44de3c349890279ba1b5fcc4dae7c46f2b
Gervig <gervig91@gmail.com>
2024-10-22 10:53:18 +0200

    Update cupcake.sql

--- src/main/resources/sql/cupcake.sql
+++ src/main/resources/sql/cupcake.sql
@@ -48,3 +48,23 @@ CREATE TABLE OrderLine
     bottom_id    INT REFERENCES Bottoms (bottom_id),                 -- Reference to the Bottoms table
     quantity     INT NOT NULL CHECK (quantity > 0)                   -- Quantity of the same cupcake, must be positive
 );
+
+-- Inserting data into the Bottoms table
+INSERT INTO Bottoms (bottom_name, price)
+VALUES ('Chocolate', 5.00),
+       ('Vanilla', 5.00),
+       ('Nutmeg', 5.00),
+       ('Pistacio', 6.00),
+       ('Almond', 7.00);
+
+-- Inserting data into the Toppings table
+INSERT INTO Toppings (topping_name, price)
+VALUES ('Chocolate', 5.00),
+       ('Blueberry', 5.00),
+       ('Rasberry', 5.00),
+       ('Crispy', 6.00),
+       ('Strawberry', 6.00),
+       ('Rum/Raisin', 7.00),
+       ('Orange', 8.00),
+       ('Lemon', 8.00),
+       ('Blue cheese', 9.00);
