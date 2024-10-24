0b73eeae18ced1ddee81506a090fb3983e9a69a0
Gervig <gervig91@gmail.com>
2024-10-24 13:36:31 +0200

    Update OrderlineMapper.java

--- src/main/java/app/persistence/OrderlineMapper.java
+++ src/main/java/app/persistence/OrderlineMapper.java
@@ -16,6 +16,8 @@ public class OrderlineMapper
     {
         List<Orderline> orderlineList = new ArrayList<>();
 
+        // SQL join query that joins the orderline and orders tables,
+        // so you can search orderlines for a given user
         String sql = "SELECT Orderline.orderline_id, " +
                 "Orderline.order_id, " +
                 "Orderline.bottom_id, " +
@@ -23,7 +25,14 @@ public class OrderlineMapper
                 "Orderline.topping_id, " +
                 "Topping.topping_name, " +
                 "Orderline.quantity, " +
-                "Orderline.price FROM Orderline JOIN Orders ON Orderline.order_id = Orders.order_id JOIN Users ON Orders.user_id = Users.user_id JOIN Bottom ON Orderline.bottom_id = Bottom.bottom_id JOIN Topping ON Orderline.topping_id = Topping.topping_id WHERE Users.user_id = :user_id;";
+                "Orderline.price " +
+                "FROM Orderline " +
+                "JOIN Orders " +
+                "ON Orderline.order_id = Orders.order_id " +
+                "JOIN Users ON Orders.user_id = Users.user_id " +
+                "JOIN Bottom ON Orderline.bottom_id = Bottom.bottom_id " +
+                "JOIN Topping ON Orderline.topping_id = Topping.topping_id " +
+                "WHERE Users.user_id = :user_id;";
 
         try(
                 Connection connection = connectionPool.getConnection();
