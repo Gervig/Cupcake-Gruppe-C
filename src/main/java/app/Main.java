9bbb9a27205ee6fce53686f460261f384f16f37d
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-10-25 13:20:50 +0200

    Lavet en admin så vi kan have en tilbage knap

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -35,6 +35,8 @@ public class Main
         app.post("/createuser", ctx -> UserController.createUser(ctx, connectionPool));
 
         //Admin side
+        app.get("/admin", ctx -> ctx.render("admin.html"));
+
         app.get("/listOfOrders", ctx -> ctx.render("listOfOrders.html"));
         app.post("/listOfOrders", ctx -> OrderController.getAllOrders(ctx, connectionPool));
 
