cc2f18044f449bf7f9f352b82ca30678591da42f
Daniel <Liquidly@live.dk>
2024-10-25 12:31:13 +0200

    CupcakeService og CupcakeController tilføjet

--- src/main/java/app/entities/Toppings.java
+++ src/main/java/app/entities/Toppings.java
@@ -1,4 +1,30 @@
 package app.entities;
 
-public class Toppings {
+public class Toppings
+{
+    private int toppingsID;
+    private String toppingsName;
+    private float price;
+
+    public Toppings(int toppingsID, String toppingsName, float price)
+    {
+        this.toppingsID = toppingsID;
+        this.toppingsName = toppingsName;
+        this.price = price;
+    }
+
+    public int getToppingsID()
+    {
+        return toppingsID;
+    }
+
+    public String getToppingsName()
+    {
+        return toppingsName;
+    }
+
+    public float getPrice()
+    {
+        return price;
+    }
 }
