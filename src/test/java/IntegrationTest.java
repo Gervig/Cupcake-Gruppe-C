25683b1287db97380ff77d241e04ee6c6d9f2b33
Gervig <gervig91@gmail.com>
2024-10-28 10:38:59 +0100

    lalala

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,7 +1,15 @@
+import app.entities.User;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 public class IntegrationTest
 {
-
+    boolean actual;
+    boolean expected;
+    IntegrationTest it = new IntegrationTest();
+    BigDecimal bd = new BigDecimal(500.00);
+    User u1 = new User(1, "Casper", "Gervig", "gervig91", "gervig@gmail.com", "1234", bd, 2400, "customer");
 }
