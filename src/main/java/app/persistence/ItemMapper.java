061741526fab2d546f4ce3dbd63d9facef79c359
Gervig <gervig91@gmail.com>
2024-10-29 12:12:56 +0100

    fixed create order

--- src/main/java/app/persistence/ItemMapper.java
+++ src/main/java/app/persistence/ItemMapper.java
@@ -121,7 +121,7 @@ public class ItemMapper
 
     private static int findActiveOrderId(Connection connection, int userId) throws DatabaseException
     {
-        String sql = "INSERT INTO public.Orderline (order_id, bottom_id, topping_id, quantity, price) VALUES (?, ?, ?, ?, ?)";
+        String sql = "SELECT * FROM orders WHERE user_id = ?";
 
         try (PreparedStatement ps = connection.prepareStatement(sql))
         {
@@ -168,9 +168,6 @@ public class ItemMapper
         return (selectedBottom.getPrice().add(selectedTopping.getPrice())).multiply(BigDecimal.valueOf(quantity));
     }
 
-
-
-
     public static Bottoms getBottomById(int bottomId, ConnectionPool connectionPool) throws DatabaseException
     {
         Bottoms bottom = null;
@@ -185,7 +182,10 @@ public class ItemMapper
 
             if (rs.next())
             {
-                bottom = new Bottoms(rs.getInt("id"), rs.getString("name"), rs.getBigDecimal("price"));
+                int id = rs.getInt("bottom_id");
+                String name = rs.getString("bottom_name");
+                BigDecimal price = rs.getBigDecimal("price");
+                bottom = new Bottoms(id, name, price);
             }
 
         } catch (SQLException e)
@@ -209,7 +209,10 @@ public class ItemMapper
             {
                 if (rs.next())
                 {
-                    topping = new Toppings(rs.getInt("id"), rs.getString("name"), rs.getBigDecimal("price"));
+                    int id = rs.getInt("topping_id");
+                    String name = rs.getString("topping_name");
+                    BigDecimal price = rs.getBigDecimal("price");
+                    topping = new Toppings(id, name, price);
                 }
             }
         } catch (SQLException e)
